1d5c9cfc09786713a03f8c6c93dd73b741426420
TariqAHassan <tariqaahassan@gmail.com>
2016-09-01 21:06:52 -0300

    ECB Interface Bug Fixes

--- easymoney/ecb_interface.py
+++ easymoney/ecb_interface.py
@@ -28,8 +28,12 @@ def _soup_from_url(url, parser = 'lxml'):
     """
 
     *Private Method*
-    :param url: URL to parse with BeautifulSoup
+    Parses a URL with bs4's BeautifulSoup() method.
+
+    :param url: URL to parse with BeautifulSoup().
     :type url: str
+    :param parser: the parser to be used by BeautifulSoup(). Defaults to 'lxml'.
+    :type parser: str
     :return: BeautifulSoup
     :rtype: bs4 obj
     """
@@ -42,7 +46,8 @@ def ecb_exchange_data(return_as = 'dict', ecb_extension = "/stats/eurofxref/euro
     """
 
     | This tool goes out to the European Central Bank via their generously provided API
-    | and coerces XML data into a dictionary.
+    | and coerces XML data into a dictionary. Expects 'time', 'currency', 'rate' in the XML data.
+    | Returns a nested dictionary of the form: {time: {currency: rate}}.
     | DO NOT WRITE PROCEDURES THAT SLAM THEIR SERVERS.
 
     :param return_as: 'dict' for dictionary (nested); 'df' for pandas dataframe.
@@ -53,13 +58,17 @@ def ecb_exchange_data(return_as = 'dict', ecb_extension = "/stats/eurofxref/euro
     :return: exchange rate with EUR as the base-currency.
     :rtype: dict or Pandas DataFrame
     """
-    xmlpath =  "http://www.ecb.europa.eu" + ecb_extension
+    # Initialize
+    all_currency_codes = list()
 
-    # Parse with beautifulsoup
+    # Constuct the URL to the XML data on the ECB's website.
+    xmlpath = "http://www.ecb.europa.eu" + ecb_extension
+
+    # Parse with BeautifulSoup
     soup = _soup_from_url(xmlpath)
 
     # Convert to string
-    ecb_hist_xml = str(soup.find_all("cube", attrs = {"time" : True})).split(",")
+    ecb_hist_xml = str(soup.find_all("cube", attrs = {"time": True})).split(",")
 
     # Get all Dates
     exchange_rate_db = dict.fromkeys([re.findall(r'time="(.*?)">', i)[0] for i in ecb_hist_xml])
@@ -67,17 +76,17 @@ def ecb_exchange_data(return_as = 'dict', ecb_extension = "/stats/eurofxref/euro
     # Extract time, currency and rate information
     for j in ecb_hist_xml:
         date = re.findall(r'time="(.*?)">', j)[0]
-        ccode = re.findall(r'currency="(.*?)" rate=', j)
-        rate = re.findall(r'rate="(.*?)">', j)
-        exchange_rate_db[date] = dict(zip(ccode, [floater(x) for x in rate]))
+        ccodes = re.findall(r'currency="(.*?)" rate=', j)
+        rates = re.findall(r'rate="(.*?)">', j)
+        exchange_rate_db[date] = dict(zip(ccodes, [floater(x) for x in rates]))
+        all_currency_codes += [c for c in ccodes if c not in all_currency_codes]
 
     # return as dict
     if return_as == 'dict':
-        return exchange_rate_db, ccode
+        return exchange_rate_db, all_currency_codes
 
     # return as pandas df
     elif return_as == 'df':
-
         # Convert to pandas dataframe, convert date --> column and reindex
         df = pd.DataFrame.from_dict(exchange_rate_db, orient = 'index')
         df['date'] = df.index
@@ -90,7 +99,7 @@ def ecb_exchange_data(return_as = 'dict', ecb_extension = "/stats/eurofxref/euro
         # Convert date column --> datetime
         df.date = pd.to_datetime(df.date, infer_datetime_format = True)
 
-        return df, ccode
+        return df, all_currency_codes
 
 ecb_currency_to_alpha2_dict = {   "CYP": "CY"
                                 , "EEK": "EE"
