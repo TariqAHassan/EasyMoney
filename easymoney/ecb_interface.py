2de4f46d4c8448c5c2e3e6962a74ab0e727a764d
TariqAHassan <tariqaahassan@gmail.com>
2016-09-02 18:46:43 -0300

    Large ECB Interface Speed Improvement

--- easymoney/ecb_interface.py
+++ easymoney/ecb_interface.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 """
 
     Tools for Obtaining Data from the European Central Bank
@@ -11,38 +10,12 @@
 
 # Modules #
 import re
-import sys
+import requests
 import pandas as pd
-from bs4 import BeautifulSoup
 from easymoney.support_money import floater
 
-if sys.version_info.major == 3:
-    import urllib.request
-elif sys.version_info.major == 2 and sys.version_info.minor >= 7:
-    import urllib2
-else:
-    raise ImportError("Your Version of Python is too old. Please Consider upgrading to --at least-- Python 2.7.\n"
-                      "Python 3.5 is preferable.")
 
-def _soup_from_url(url, parser = 'lxml'):
-    """
-
-    *Private Method*
-    Parses a URL with bs4's BeautifulSoup() method.
-
-    :param url: URL to parse with BeautifulSoup().
-    :type url: str
-    :param parser: the parser to be used by BeautifulSoup(). Defaults to 'lxml'.
-    :type parser: str
-    :return: BeautifulSoup
-    :rtype: bs4 obj
-    """
-    if sys.version_info.major == 3:
-        return BeautifulSoup(urllib.request.urlopen(url), parser)
-    else:
-        return BeautifulSoup(urllib2.Request(url), parser)
-
-def ecb_exchange_data(return_as = 'dict', ecb_extension = "/stats/eurofxref/eurofxref-hist.xml"):
+def ecb_xml_exchange_data(return_as = 'dict', ecb_extension = "/stats/eurofxref/eurofxref-hist.xml"):
     """
 
     | This tool goes out to the European Central Bank via their generously provided API
@@ -64,20 +37,20 @@ def ecb_exchange_data(return_as = 'dict', ecb_extension = "/stats/eurofxref/euro
     # Constuct the URL to the XML data on the ECB's website.
     xmlpath = "http://www.ecb.europa.eu" + ecb_extension
 
-    # Parse with BeautifulSoup
-    soup = _soup_from_url(xmlpath)
+    # Request the data from the sever.
+    url_request = requests.get(xmlpath)
 
-    # Convert to string
-    ecb_hist_xml = str(soup.find_all("cube", attrs = {"time": True})).split(",")
+    # Convert the XML to str, split on 'Cube><Cube' and remove the first element.
+    parsed_xml = str(url_request.content).split("Cube><Cube")[1:]
 
-    # Construct Dict
+    # Initialize the exchange rate dict
     exchange_rate_db = dict()
 
-    # Extract time, currency and rate information
-    for j in ecb_hist_xml:
-        date = re.findall(r'time="(.*?)">', j)[0]
+    # Iterate though the parsed XML
+    for j in parsed_xml:
+        date = re.findall(r'time="(.*?)"', j)[0]
         ccodes = re.findall(r'currency="(.*?)" rate=', j)
-        rates = re.findall(r'rate="(.*?)">', j)
+        rates = re.findall(r'rate="(.*?)"', j)
         exchange_rate_db[date] = dict(zip(ccodes, [floater(x) for x in rates]))
         all_currency_codes += [c for c in ccodes if c not in all_currency_codes]
 
@@ -117,3 +90,5 @@ ecb_currency_to_alpha2_dict = {   "CYP": "CY"
                                 , "SKK": "SK"
                                 , "TRL": "TR"
 }
+
+ecb_xml_exchange_data('df')[0]
