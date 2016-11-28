2187e24859b55b18d66f5ebc1bc7a56306a3a4e4
TariqAHassan <tariqaahassan@gmail.com>
2016-11-28 04:43:55 -0400

    Update Support Tools

--- easymoney/support_tools.py
+++ easymoney/support_tools.py
@@ -2,13 +2,13 @@
 
 """
 
-    General Support Tools
-    ~~~~~~~~~~~~~~~~~~~~~
+    Support Tools
+    ~~~~~~~~~~~~~
 
 """
-
-# modules
+# Imports
 import re
+import pandas as pd
 import dateutil.parser
 from datetime import datetime
 
@@ -24,7 +24,6 @@ def cln(i, extent = 1):
     :param extent: 1 --> all white space reduced to length 1; 2 --> removal of all white space.
     :return:
     """
-
     if isinstance(i, str) and i != "":
         if extent == 1:
             return re.sub(r"\s\s+", " ", i)
@@ -35,6 +34,26 @@ def cln(i, extent = 1):
 
 def min_max(iterable):
     return (min(iterable), max(iterable))
+
+def sort_range_reverse(iterable, action='range'):
+    if not isinstance(iterable, list):
+        return iterable
+    elif action == 'sort':
+        return sorted(iterable)
+    elif action == 'reverse':
+        return list(reversed(iterable))
+    elif action == 'range':
+        return list(min_max(iterable))
+    else:
+        return iterable
+
+# ---------------------------------------------------------------------------------
+# Mathematical
+# ---------------------------------------------------------------------------------
+
+def closest_value(value, list_of_values):
+    # Source: http://stackoverflow.com/a/12141207/4898004
+    return min(list_of_values, key=lambda i: abs(i - value))
 
 # ---------------------------------------------------------------------------------
 # Dates
@@ -49,10 +68,41 @@ def date_reformat(date, to_format='%d/%m/%Y', from_format=None):
         return date
     return datetimer(date, from_format).strftime(to_format)
 
-def min_max_dates(list_of_dates, from_format="%d/%m/%Y"):
-    datetimes = [datetimer(d, from_format) for d in list_of_dates]
-    return tuple([d.strftime(from_format) for d in min_max(datetimes)])
+def date_sort(list_of_dates, from_format="%d/%m/%Y"):
+    return sorted(list_of_dates, key=lambda x: datetime.strptime(x, from_format))
 
+def min_max_dates(list_of_dates, from_format="%d/%m/%Y"):
+    pandas_datetime = pd.to_datetime(pd.Series(list_of_dates), format=from_format)
+    return (pandas_datetime.min().strftime(from_format), pandas_datetime.max().strftime(from_format))
+
+def closest_date(date, list_of_dates, from_format="%d/%m/%Y"):
+    list_of_datetimes = [datetimer(d, from_format) for d in list_of_dates]
+    closest = closest_value(datetimer(date, from_format), list_of_datetimes)
+    return closest.strftime(from_format)
+
+def _canonical_datetime(date_format):
+    for f, c in zip(['%d', '%m', '%Y'], ['DD', 'MM', 'YYYY']):
+        date_format = date_format.replace(f, c)
+    return date_format
+
+def date_format_check(date, from_format="%d/%m/%Y"):
+    try:
+        datetime.strptime(date, from_format)
+        return True
+    except ValueError:
+        raise ValueError("Invalid date format.\n"
+                         "Please supply a date of the form: %s." % (_canonical_datetime(from_format)))
+
+def year_extract(date):
+    year = ""
+    for c in str(date):
+        if len(year) == 4:
+            break
+        if c.isdigit():
+            year += c
+        elif not c.isdigit() and len(year) < 4:
+            year = ""
+    return year if len(year) == 4 else None
 
 # ---------------------------------------------------------------------------------
 # Money Formatting
@@ -66,6 +116,13 @@ def mint(amount, currency='', pretty_print=False):
         print(money_formater(amount, currency=currency))
     else:
         return float(amount)
+
+
+
+
+
+
+
 
 
 
