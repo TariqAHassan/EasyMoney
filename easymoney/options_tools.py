2469fb0cfe9f289380faf9f9e027aede82d04ff2
TariqAHassan <tariqaahassan@gmail.com>
2016-11-28 19:18:07 -0400

    Add Options Tool; Update Docstrings

--- easymoney/options_tools.py
+++ easymoney/options_tools.py
@@ -7,7 +7,10 @@
 
 """
 # Imports
+from datetime import datetime
+
 from easymoney.easy_pandas import items_null
+from easymoney.support_tools import date_sort
 
 def options_ranking(inflation, exchange):
     """
@@ -19,9 +22,9 @@ def options_ranking(inflation, exchange):
         1: inflation information
         0: neither inflation or exchange information
 
-    :param inflation:
+    :param inflation: date rage of data available for inflation data.
     :type inflation: ``list``
-    :param exchange:
+    :param exchange: date rage of data available for exchange data.
     :type exchange: ``list``
     :return: completeness of row
     :rtype: ``int``
@@ -35,6 +38,24 @@ def options_ranking(inflation, exchange):
     else:
         return 0
 
+def year_date_overlap(years, full_dates, date_format="%d/%m/%Y"):
+    """
+
+    Get the overlap for a iterable of years and an iterable of full dates (as strings).
+
+
+    :param range_a: years of the form (year_a, year_b). Length must be equal to 2.
+    :type range_a: iterable
+    :param range_b: dates of the form (string_date_a, string_date_b). Length must be equal to 2.
+    :type range_b: iterable
+    :return: overlap
+    :rtype: tuple
+    """
+    year_datetimes = [datetime.strptime(dm + str(y), date_format) for y, dm in zip(sorted(years), ["01/01/", "31/12/"])]
+    dates_datetimes = [datetime.strptime(d, date_format) for d in date_sort(full_dates, from_format=date_format)]
+    date_floor = max([year_datetimes[0], dates_datetimes[0]])
+    date_ceiling = min([year_datetimes[1], dates_datetimes[1]])
+    return date_floor.strftime(date_format), date_ceiling.strftime(date_format)
 
 
 
