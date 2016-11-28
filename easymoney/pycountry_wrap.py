71dc194819aa0fb7a0c3bab9b1ed4c5b7495af61
TariqAHassan <tariqaahassan@gmail.com>
2016-11-28 15:15:16 -0400

    Improve Docstrings

--- easymoney/pycountry_wrap.py
+++ easymoney/pycountry_wrap.py
@@ -15,16 +15,21 @@ from easymoney.sources.databases import currency_mapping_to_dict
 class PycountryWrap(object):
     """
 
+    Tools wrapping the `pycountry` module.
 
-    :param fuzzy_threshold:
-    :param data_path:
+    :param fuzzy_match_threshold: a threshold for fuzzy matching confidence (requires the ``fuzzywuzzy`` package).
+                                  For more, see ``EasyPeasy()`` in the `money` module.
+    :type fuzzy_threshold: ``int`` or ``float``
+    :param data_path: path to the database file(s). Defaults to ''.
+    :type data_path: ``str``
     """
+
 
     def __init__(self, fuzzy_threshold=False, data_path=''):
         """
 
-        :param fuzzy_threshold:
-        :param data_path:
+        Initialize the ``PycountryWrap()`` class.
+
         """
         # Compute the dict mapping alpha2 codes to currencies
         self.alpha2_currency_dict = currency_mapping_to_dict(data_path)
@@ -45,12 +50,20 @@ class PycountryWrap(object):
                                   " - python 3: $ pip3 install fuzzywuzzy\n"
                                   "You may need to create a new Python instance for these changes to take effect.")
 
+
     def _country_extract(self, country, extract_type='alpha_2'):
         """
 
-        :param country: a pycountry object
-        :param extract_type:
-        :return:
+        Extract information from a given country in the `pycountry` database.
+
+        :param country: a pycountry object to harvest `extract_type` from.
+                        Recognized types of information: 'alpha_2', 'alpha_3', 'numeric', 'name' or 'official_name'.
+
+        :type country: ``pycountry object``
+        :param extract_type: The type of information to extract from `country`. Defaults to 'alpha_2'.
+        :type extract_type: ``str``
+        :return: a country's ISO alpha 2 code, ISO alpha 3 code, numeric code, name or official name.
+        :rtype: ``str`` or ``None``
         """
         if extract_type == 'alpha_2':
             try:
@@ -83,14 +96,19 @@ class PycountryWrap(object):
         else:
             raise ValueError("invalid extract_type supplied")
 
+
     def _currency_extract(self, currency_name, extract_type='currency_alpha_3'):
         """
 
-        :param currency_name:
-        :param extract_type:
-        :return:
-        """
+        Extract information from a given currency in the `pycountry` database.
 
+        :param currency_name: a country.
+        :type currency_name: ``str``
+        :param extract_type: Defaults to 'currency_alpha_3'.
+        :type extract_type: ``str``
+        :return: an ISO Alpha 3 currency code.
+        :rtype: ``pycountry object`` or ``None``
+        """
         if extract_type == 'currency_alpha_3':
             try:
                 return pycountry.currencies.get(alpha_3=currency_name).alpha_3
@@ -110,21 +128,32 @@ class PycountryWrap(object):
         else:
             raise ValueError("invalid extract_type supplied")
 
+
     def _fuzzy_search(self, term, options):
         """
+        
+        Fuzzy Searching tool via the `fuzzywuzzy` module.
 
-        :param term:
-        :param options:
-        :return:
+        :param term: a search term.
+        :type term: ``str``
+        :param options: an iterable of options to match on.
+        :type options: ``iterable``
+        :return: best match match quality is less than the threshold.
+        :rtype: ``str``
         """
         rslt = self.extractOne(term, options)
         return rslt[0] if rslt[1] >= self.fuzzy_threshold else None
 
-    def _country_lookup(self, region):
-        """
 
-        :param region:
-        :return:
+    def _region_lookup(self, region):
+        """
+        
+        Tool to find a region's `pycountry` object.
+
+        :param region: a region.
+        :type region: ``str``
+        :return: a `pycountry` object for `region`.
+        :rtype: ``pycountry object`` or ``None``
         """
         fuzzy_match = None
         try:
@@ -139,18 +168,22 @@ class PycountryWrap(object):
             else:
                 return None
 
+
     def map_region_to_type(self, region, extract_type='alpha_2'):
         """
 
         Maps the input region to:
-            region: alpha_2, alpha_3, name, official_name
-            currency: currency_alpha_3, currency_numeric, currency_name
+            region: 'alpha_2', 'alpha_3', 'name' or 'official_name'.
+            currency: 'currency_alpha_3', 'currency_numeric' or 'currency_name'.
 
         :param region: any region
-        :param extract_type:
-        :return:
+        :type region: ``str``
+        :param extract_type: the type of information about `region` to extract. Must be one of the types listed above.
+        :type extract_type: ``str``
+        :return: `extract_type` information.
+        :rtype: ``str`` or ``None``
         """
-        rslt = self._country_lookup(region)
+        rslt = self._region_lookup(region)
         if rslt is None:
             return None
 
