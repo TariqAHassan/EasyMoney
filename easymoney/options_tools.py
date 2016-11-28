9d2fe041a4b715305efb8bd99f35e1af16317fd7
TariqAHassan <tariqaahassan@gmail.com>
2016-11-28 15:55:44 -0400

    Improve Docstrings and Remove Deprecated Function

--- easymoney/options_tools.py
+++ easymoney/options_tools.py
@@ -6,36 +6,25 @@
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
-
+# Imports
 from easymoney.easy_pandas import items_null
-from easymoney.support_tools import min_max_dates
-
-def multi_currency_dates(list_of_currencies, _exchange_dates, get_range=False):
-    """
-
-    :param list_of_currencies:
-    :param _exchange_dates: method
-    :param get_range:
-    :return:
-    """
-    currency_dates = list(filter(None, map(_exchange_dates, list_of_currencies)))
-
-    # Return if all sublists are null
-    if not len(currency_dates):
-        return None
-
-    if get_range:
-        currency_dates = [list(min_max_dates(l)) for l in currency_dates]
-
-    return currency_dates[0] if len(currency_dates) == 1 else currency_dates
-
 
 def options_ranking(inflation, exchange):
     """
 
+    Ranks ``options()`` table rows by their completeness.
+    Rankings:
+        3: inflation and exchange information
+        2: exchange information
+        1: inflation information
+        0: neither inflation or exchange information
+
     :param inflation:
+    :type inflation: ``list``
     :param exchange:
-    :return:
+    :type exchange: ``list``
+    :return: completeness of row
+    :rtype: ``int``
     """
     if not items_null(inflation) and not items_null(exchange):
         return 3
