1903c2e65043860120cf41697df61d87441dc97b
TariqAHassan <tariqaahassan@gmail.com>
2016-11-30 16:48:22 -0400

    Version 1.5

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import os
-
 from setuptools import setup, find_packages
-from warnings import warn
+
 
 def read(fname):
     try:
@@ -9,25 +8,26 @@ def read(fname):
     except:
         return 'Please see: https://github.com/TariqAHassan/EasyMoney.'
 
+
 setup(
     name = "easymoney",
-    version = "1.0.0",
+    version = "1.5.0",
     author = "Tariq A. Hassan",
     author_email = "laterallattice@gmail.com",
-    description = ("Tools for Monetary Information and Conversions."),
+    description = ("Data Science Tools for Monetary Information and Conversions."),
     long_description = read('docs/README.rst'),
     license = "BSD",
     keywords = 'economics, finance, inflation, currency converter, data analysis, data science',
     url = "https://github.com/TariqAHassan/EasyMoney.git",
-    download_url='https://github.com/TariqAHassan/EasyMoney/archive/1.0.0.tar.gz',
-    packages = find_packages(exclude = [  "tests.*"
+    download_url='https://github.com/TariqAHassan/EasyMoney/archive/1.5.0.tar.gz',
+    packages = find_packages(exclude = ["tests.*"
                                         , "tests"
                                         , "sources/single_use.*"
                                         , "sources/single_use"
                                         ]),
     package_data = {'easymoney': ['sources/data/*.csv'],},
     data_files = [('', ["LICENSE.txt"])],
-    install_requires = ['numpy', 'pandas', 'requests', 'wbdata', 'pycountry'],
+    install_requires = ['numpy', 'pandas', 'pycountry', 'requests', 'wbdata'],
     classifiers = ["Development Status :: 5 - Production/Stable"
                    , "Natural Language :: English"
                    , "Intended Audience :: Science/Research"
@@ -36,12 +36,9 @@ setup(
                    , "Programming Language :: Python :: 3.3"
                    , "Programming Language :: Python :: 3.4"
                    , "Programming Language :: Python :: 3.5"
-                   , "Environment :: MacOS X"
                    , "License :: OSI Approved :: BSD License"
     ],
     include_package_data = True
 )
-
-
 
 
